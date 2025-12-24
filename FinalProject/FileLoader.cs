c72d034c516ce4a214cd1de105631fa429b99995
IsaacWilliston <ilyaserbin109@gmail.com>
2025-12-24 17:19:41 +0500

    Basic classes

---
+++ FinalProject/FileLoader.cs
@@ -0,0 +1,23 @@
+﻿namespace FinalProject;
+
+class FileLoader
+{
+    public static List<Product> LoadProducts(string path)
+    {
+        var products = new List<Product>();
+
+        foreach (var line in File.ReadLines(path))
+        {
+            var parts = line.Split(';');
+            products.Add(new Product(
+                int.Parse(parts[0]),
+                parts[1],
+                parts[2],
+                decimal.Parse(parts[3]),
+                int.Parse(parts[4])
+            ));
+        }
+
+        return products;
+    }
+}
---
+++ FinalProject/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalProject;
+
+class Product
+{
+    public int Id { get; }
+    public string Name { get; }
+    public string Category { get; }
+    public decimal Price { get; }
+    public int Quantity { get; }
+
+    public Product(int id, string name, string category, decimal price, int quantity)
+    {
+        Id = id;
+        Name = name;
+        Category = category;
+        Price = price;
+        Quantity = quantity;
+    }
+}
