23fe2fcb62bb0901e4fd8f68814d1f23ae0862f8
IsaacWilliston <ilyaserbin109@gmail.com>
2025-12-25 17:22:11 +0500

    v1.3 FileProductRepository fix, single function for displaying search results

--- FinalProject/Controllers/ProductController.cs
+++ FinalProject/Controllers/ProductController.cs
@@ -1,35 +1,16 @@
-﻿namespace FinalProject.Controllers;
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace FinalProject.Controllers;
 using Domain;
 using Services;
 
-public class ProductController 
-{
+public class ProductController {
     private readonly InventoryService _inventory;
     public ProductController(InventoryService inventory) => _inventory = inventory;
 
-    public List<Product> SearchByName(string? term)
-    {
-        if (string.IsNullOrWhiteSpace(term)) return new List<Product>();
-        return _inventory.SearchByName(term);
-    }
-
-    public List<Product> SearchById(int id)
-    {
-        if (id == 0) return new List<Product>();
-        return _inventory.SearchById(id);
-    }
-
-    public List<Product> SearchByCategory(string name)
-    {
-        if (string.IsNullOrWhiteSpace(name)) return new List<Product>();
-        return _inventory.SearchByCategory(name);
-    }
-
-    public List<Product> SearchByPrice(decimal price)
-    {
-        if (price == 0) return new List<Product>();
-        return _inventory.SearchByPrice(price);
-    }
-
     public List<Product> GetAllProducts() => _inventory.GetAll();
+    public List<Product> SearchByName(string term) => _inventory.SearchByName(term);
+    public List<Product> SearchByCategory(string term) => _inventory.SearchByCategory(term);
+    public List<Product> SearchById(int id) => _inventory.SearchById(id);
+    public List<Product> SearchByPrice(decimal price) => _inventory.SearchByPrice(price);
 }
